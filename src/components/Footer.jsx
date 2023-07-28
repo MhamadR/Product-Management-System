4efc7bb3a30456e78bf245b9d89f90702073f130
Mhamad Raslan <mhamadraslan@gmail.com>
2023-07-28 15:45:13 +0300

    Add Footer component with style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../Styles/Footer.scss";
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Scandiweb Test Assignment</p>
+    </footer>
+  );
+}
+
+export default Footer;
