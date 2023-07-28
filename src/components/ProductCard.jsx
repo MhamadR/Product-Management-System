37ecf5256bd02a1126aa34f91852410a432e1bc8
Mhamad Raslan <mhamadraslan@gmail.com>
2023-07-28 15:45:33 +0300

    Add ProductCard component with style

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,53 @@
+import PropTypes from "prop-types";
+import "../Styles/ProductCard.scss";
+
+function ProductCard({
+  id,
+  sku,
+  name,
+  price,
+  property,
+  value,
+  onCheckBoxChange,
+}) {
+  function handleCheckBoxChange(e) {
+    onCheckBoxChange(e);
+  }
+
+  return (
+    <div className="product-card">
+      <input
+        type="checkbox"
+        className="delete-checkbox"
+        name={name}
+        id={id}
+        onChange={handleCheckBoxChange}
+      />
+      <label htmlFor={id}>
+        <p>{sku} </p>
+        <p> {name}</p>
+        <p> {price} $ </p>
+        <p>
+          {property}: {value}
+        </p>
+      </label>
+    </div>
+  );
+}
+
+// Defining PropTypes in order to solve ESLint error: is missing in props validationeslintreact/prop-types)
+ProductCard.propTypes = {
+  id: PropTypes.number,
+  sku: PropTypes.string,
+  name: PropTypes.string,
+  price: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  property: PropTypes.string,
+  value: PropTypes.string,
+  onCheckBoxChange: PropTypes.func,
+};
+
+// ProductCard.defaultProps = {
+//   id: "",
+// };
+
+export default ProductCard;
