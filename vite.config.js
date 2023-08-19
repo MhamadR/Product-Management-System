e03d7d381d35c06e93f67f8d2dda641c7ef30938
Mhamad Raslan <mhamadraslan@gmail.com>
2023-08-19 18:26:08 +0300

    Add documentation

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://antiwar-containers.000webhostapp.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Remove the "/api" prefix
+        target: "https://antiwar-containers.000webhostapp.com/",
+        changeOrigin: true, // Change the Origin header in the request to match the target server's origin.
+        rewrite: (path) => path.replace(/^\/api/, ""), // Remove the "/api" prefix from the path before making the request
       },
     },
   },
