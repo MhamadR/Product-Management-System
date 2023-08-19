2ed874bdda84334f3c1969bb589fd799d09e8783
Mhamad Raslan <mhamadraslan@gmail.com>
2023-08-19 22:15:08 +0300

    Add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "../Styles/Button.scss";
+
+function Button({ to, element, value, onClick, ...props }) {
+  const handleClick = () => {
+    onClick();
+  };
+
+  if (to) {
+    return (
+      <Link to={to} {...props}>
+        {value}
+      </Link>
+    );
+  } else if (element === "input") {
+    return <input value={value} onClick={handleClick} {...props} />;
+  } else {
+    return (
+      <button onClick={handleClick} {...props}>
+        {value}
+      </button>
+    );
+  }
+}
+
+export default Button;
+
+Button.propTypes = {
+  to: PropTypes.string,
+  element: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
